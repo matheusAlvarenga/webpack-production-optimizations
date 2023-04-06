c47d3c9217c7a106bf89d8db3bf19296b3264d0a
Matheus Alvarenga <matheus.alvarenga0302@gmail.com>
2023-04-06 20:58:59 -0300

    chore: install and configure webpack dev server

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -1,6 +1,21 @@
 const common = require('./webpack.common.config.js')
+const path = require('path')
 const { merge } = require('webpack-merge') 
 
 module.exports = merge(common, {
   mode: 'development',
+  devServer: {
+    port: '3000',
+    static: {
+      directory: path.resolve(__dirname, '..')
+    },
+    devMiddleware: {
+      index: 'index.html',
+      writeToDisk: true
+    },
+    client: {
+      overlay: true
+    },
+    liveReload: false
+  }
 })
