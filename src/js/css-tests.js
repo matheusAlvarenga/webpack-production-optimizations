24366d32374431f870f8c3704c424fd9e8cca891
Matheus Alvarenga <matheus.alvarenga0302@gmail.com>
2023-04-12 19:23:10 -0300

    chore: add jss for testing

--- src/js/css-tests.js
+++ src/js/css-tests.js
@@ -1,10 +1,26 @@
 import '../styles/less-testing.less'
 import '../styles/sass-testing.scss'
 
+import jss from 'jss'
+import preset from 'jss-preset-default'
+
+jss.setup(preset())
+
+const jssStyles = {
+  'jssTests': {
+    backgroundColor: 'green',
+    fontSize: '20px',
+    color: 'white'
+  }
+}
+
+const { classes } = jss.createStyleSheet(jssStyles).attach()
+
 export function renderCssTests() {
   const tests = [
     `<div class='less-tests'>This is a test with less preprocessor</div>`,
     `<div class='sass-tests'>This is a test with sass preprocessor</div>`,
+    `<div class=${classes.jssTests}>This is a test with jss</div>`,
   ]
 
   document.body.innerHTML += `<div class='tests-wrapper'>${tests.join('')}</div>`
